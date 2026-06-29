5ba621c7bd83893ab8173520d81bbe75ebba4672
Robert Konecny <rokorobot@users.noreply.github.com>
2026-06-29 18:27:00 +0200

    feat: render Intro inside router Routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SettingsProvider } from "@/hooks/useSettings";
 import { AuthProvider } from "@/context/AuthContext";
@@ -25,7 +25,9 @@ const App = () => {
             <Toaster />
             <Sonner />
             <BrowserRouter>
-              <Intro />
+              <Routes>
+                <Route path="/" element={<Intro />} />
+              </Routes>
             </BrowserRouter>
           </TooltipProvider>
         </AuthProvider>
